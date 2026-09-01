c2ce28c568f4addf8048232d027da0ffed58515a
Robertino Klug <roberttinoklugg@gmail.com>
2026-09-01 16:23:59 -0300

    Primer commit tp1 IPOO

---
+++ Libros.py
@@ -0,0 +1,59 @@
+
+
+class Libro:                        # constructor de la clase libro
+                                 #         |  
+                      # ___________________V______________________            
+    def __init__(self, isbn: int, titulo: str, autor: str, anio: int, genero: str, paginas: int):
+        self.isbn = isbn
+        self.titulo = titulo
+        self.autor = autor
+        self.anio = anio
+        self.genero = genero
+        self.paginas = paginas
+
+        self. disponible=True 
+        # Indica si el libro está disponible para préstamo
+        self. cantidad_prestamos=0 
+        # Indica cuantas veces se presto el libro
+
+# METODOS
+
+def __str__(self):
+    return f'isbn: {self.isbn} Titulo: {self.titulo} Autor: {self.autor} anio: {self.anio} Disponible: {'Si' if self.disponible else 'No'} Genero:{self.genero} Paginas: {self.paginas}
+
+def prestar(self):
+    if self.disponible:
+        self.disponible = False
+        self.cantidad_prestamos += 1
+        return True
+    else:
+        return False
+
+def devolver(self):
+    if not self.disponible:
+        self.disponible = True
+        return True
+    else:
+        return False
+
+def esta_disponible(self):
+    if self.disponible == True:
+        return True
+    else:
+        return False
+
+def cantidad_prestamos(self):
+    if self.cantidad_prestamos >0:
+        return self.cantidad_prestamos
+    else:
+        return 0
+
+def mostrar_informacion(self):
+    print(f"ISBN: {self.isbn}")
+    print(f"Título: {self.titulo}")
+    print(f"Autor: {self.autor}")
+    print(f"Año: {self.anio}")
+    print(f"Género: {self.genero}")
+    print(f"Páginas: {self.paginas}")
+    print(f"Disponible: {'Sí' if self.disponible else 'No'}")
+    print(f"Cantidad de préstamos: {self.cantidad_prestamos}")
---
+++ main.py
@@ -0,0 +1,36 @@
+from Libros import Libro
+
+
+def crear_objetos()->list:
+    libro1=Libro( isbn= 1032, titulo='Emma', autor='Austen', anio= 1815, genero= 'Romance', paginas= 474)
+    libro2=Libro(isbn= 6489, titulo='Dune', autor='Herbert', anio= 1965, genero= 'Ciencia ficcion', paginas= 688)
+    libro3=Libro(isbn= 5993, titulo='Dracula', autor='Stoker', anio= 1897, genero= 'Terror', paginas= 418)
+    libro4=Libro(isbn= 1204, titulo='Hamlet', autor='Shakespeare', anio= 1603, genero= 'Drama', paginas= 200)
+    libro5=Libro(isbn= 1244, titulo='Matilda', autor='Dahl', anio= 1988, genero= 'Fantasia', paginas= 240)
+    libro6=Libro(isbn= 5869, titulo='Carrie', autor='King', anio= 1974, genero= 'Terror', paginas= 199)
+    libro7=Libro(isbn= 2211, titulo='Momo', autor='Ende', anio= 1973, genero= 'Fantasia', paginas= 304)
+    libro8=Libro(isbn= 5674, titulo='El Hobbit', autor='Tolkien', anio= 1937, genero= 'Fantasia', paginas= 310)
+    libro9=Libro(isbn= 7965, titulo='Pinocho', autor='Collodi', anio= 1883, genero= 'Fantasia', paginas= 188)
+    libro10=Libro(isbn= 2357, titulo='Alicia', autor='Carroll', anio= 1865, genero= 'Fantasia', paginas= 128)
+    return [libro1,libro2,libro3,libro4,libro5,libro6,libro7,libro8,libro9,libro10]
+
+
+
+
+
+
+
+
+    def __main__():
+        pass
+    
+        if __name__ ==__main__:
+            __main__()
+
+libros = crear_objetos()
+    for libro in libros:
+        print(str(libro))
+
+
+
+
