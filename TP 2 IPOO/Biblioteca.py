b9fef6b3cba2cec973f56851fa16397e60997a8a
Robertino Klug <roberttinoklugg@gmail.com>
2026-09-23 23:00:27 -0300

    agrego varias cosas

--- TP 2 IPOO/Biblioteca.py
+++ TP 2 IPOO/Biblioteca.py
@@ -43,3 +43,51 @@ class Biblioteca:
         print(f"Páginas: {self.paginas}")
         print(f"Disponible: {'Sí' if self.__disponible else 'No'}")
         print(f"Cantidad de préstamos: {self.cantidad_prestamos}")
+
+    def creacion_libro(libros:list):
+    
+        isbnNEW=int(input('Ingrese el isbn del nuevo libro que quiere agregar '))
+        while len(str(isbnNEW)) >0 and len(str(isbnNEW)) <4:
+            isbnNEW=int(input('El campo ISBN debe tener 4 dígitos. Porfavor, ingrese un ISBN valido ')) 
+    
+    
+        titleNEW=str(input('Ingrese el titulo del nuevo libro que quiere agregar '))
+        while len(titleNEW) == 0:
+            titleNEW=str(input('El campo titulo no puede estar vacio. Porfavor, ingrese un titulo '))
+        if validar_titulo(titleNEW)==True:
+            libros.append(titleNEW)
+
+        autorNEW=str(input('Ingrese el autor del nuevo libro que quiere agregar '))
+
+        while len(autorNEW) == 0:
+            autorNEW=str(input('El campo autor no puede estar vacio. Porfavor, ingrese un autor '))
+        if validar_autor(autorNEW)==True:
+            libros.append(autorNEW)
+
+        pags=int(input('Ingrese la cantidad de paginas del nuevo libro que quiere agregar '))
+        while len(str(pags)) == 0 or pags <= 0:
+            pags=int(input('El campo paginas no puede ser negativo ni estar vacio. Porfavor, ingrese un numero valido '))
+        if validar_paginas(pags)==True:
+            libros.append(pags)
+
+        anioNEW=int(input('Ingrese el año del nuevo libro que quiere agregar (Mayor a 0, y no mayor al anio corriente) '))
+        while len(str(anioNEW)) == 0 or anioNEW <= 0 or anioNEW > 2026:
+            
+            anioNEW=int(input('El campo año no puede estar vacio, ser negativo o ser mayor a 2026. Porfavor, vuelva a ingresar un año valido '))
+        if validar_anio(anioNEW)==True: 
+            libros.append(anioNEW)
+
+        generoNEW=str(input('Ingrese el genero del nuevo libro que quiere agregar '))
+        while len(generoNEW) == 0:
+            generoNEW=str(input('El campo genero no puede estar vacio. Porfavor, ingrese un genero '))
+        if validar_genero(generoNEW)==True:
+            libros.append(generoNEW)
+        libronuevo=Libro(isbn=isbnNEW, titulo=titleNEW, autor=autorNEW,anio=anioNEW, paginas=pags, genero=generoNEW)
+        libros.append (libronuevo)
+
+        print (f'''Libro nuevo:
+            Titulo: {libronuevo.titulo}
+            Autor: {libronuevo.autor}
+    
+        ''')
+
--- TP 2 IPOO/PrestamoBiblioteca.py
+++ TP 2 IPOO/PrestamoBiblioteca.py
@@ -1,3 +1,6 @@
+from UsuariosBiblioteca import Usuario
+from datetime import datetime
+day=datetime.now().day
 class Prestamo:
     def __init__(self,usuario,libro,fechaPrestamo,estadoPrestamo):
         self.usuario= usuario
@@ -8,6 +11,28 @@ class Prestamo:
     def __str__(self):
         return f'Usuario: {self.usuario}, Libro: {self.libro}, Fecha de prestamo: {self.fechaPrestamo}, Estado del prestamo: {self.estadoPrestamo}'
 
+    def registrar_prestamo(self):
+        print ('---REGISTRO DE PRESTAMO---')
+        dni3=int(input('Ingrese el DNI del usuario que va a solicitar el prestamo'))
+        for dni2 in Usuario:
+            if dni3==Usuario(self.dni):
+                libro2=int(input('Ingrese el ISBN del libro que quiere solicitar'))
+                for libro2 in Libros:
+                    if libro2 in Libros:
+                        if get_disponible==True:
+                            prestamo1=Prestamo(usuario=dni3,libro=libro2,fechaPrestamo=day)
+                            #ALMACENAR EN ATRIBUTO COLEC PRESTAMO DE BIBLIOTECA
+                            
+                    else:
+                        print('No tenemos ese libro en nuestra biblioteca') 
+            else:
+                print('No se encontro ningun usuario registrado con ese DNI')        
+
+
+
+
+
     def devolucion(self):
+        pass
         
 
